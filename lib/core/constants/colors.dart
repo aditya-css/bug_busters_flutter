b7208c7f385bc46a08e96fd5d2826f1453362aef
JanakMistry2000 <janak.m@simformsolutions.com>
2022-04-02 13:19:54 +0530

    Added Structure of Profile page

--- lib/core/constants/colors.dart
+++ lib/core/constants/colors.dart
@@ -1,1 +1,8 @@
-class AppColors {}
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color kPrimary = Color(0xFF4460f1);
+  static const Color kWhite = Color(0xffffffff);
+  static const Color kBlack = Colors.black;
+  static const Color kGrey = Colors.grey;
+}
